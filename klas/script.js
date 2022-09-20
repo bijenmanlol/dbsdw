ab38f146f2002d3983fe1b71c7e4d205eba53b3a
drooler <85669046+bijenmanlol@users.noreply.github.com>
2022-09-20 19:48:23 +0200

    Add files via upload

---
+++ klas/script.js
@@ -0,0 +1,45 @@
+var myCanvas = document.createElement('canvas');
+let nummer;
+let keuze;
+let pnaam;
+
+document.getElementById("leerling").appendChild(myCanvas);
+document.getElementById("leerling").innerHTML = "KLIK HIER";
+
+let leerlingen = {
+
+  1: "NOAH",
+  2: "ARSENIY",
+  3: "HAZEER",
+  4: "LENNERT",
+  5: "HAYRULLAH",
+  6: "NASSER",
+  7: "LEVI",
+  8: "KWABENA",
+  9: "NEBI",
+  10: "CESAR",
+  11: "JULES",
+  12: "BENCE",
+  13: "XIANO",
+  14: "EDIZ",
+  15: "DRISS",
+  16: "GABRIËL"
+
+}
+
+function random() {
+  document.getElementById("leerling").disabled = true;
+  document.getElementById("leerling").innerHTML = "LOADING...";
+  nummer = Math.floor(Math.random() * (17 - 1) + 1);
+  keuze = leerlingen[nummer];
+  setTimeout(function() {
+    confetti();
+    document.getElementById("leerling").disabled = false;
+    document.getElementById("leerling").innerHTML = keuze;
+    setTimeout(function() {
+      pnaam = '<p>' + keuze + '</p>';
+      document.getElementById("leerling").innerHTML = "KLIK HIER";
+      document.getElementById("lijst").innerHTML = document.getElementById("lijst").innerHTML + pnaam
+    }, 2000)
+  }, 5000)
+}
